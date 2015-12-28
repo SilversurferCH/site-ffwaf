941c57a01e8f4f9d0576fb487e39560d38fecf4f
SilversurferCH <Kev.Ut@gmx.de>
2015-12-28 18:09:02 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -42,7 +42,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 2015.1.2-2
+DEFAULT_GLUON_RELEASE := ffwaf_master+exp$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
